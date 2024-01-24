f63e0cc8a39e03d8e82bf0465c31c9665981dc13
DESKTOP-E1FRVPN <ojal.mittal5@gmail.com>
2024-01-24 20:39:38 +0530

    second commit

--- mainTest.py
+++ mainTest.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 from PIL import Image
 import numpy as np
 
-model= load_model('BrainTumor10EpochsCategorical.h5')
+model= load_model('BrainTumor10Epochs.h5')
 
 image=cv2.imread('D:\\brain_tumor\\pred\\pred5.jpg')
 
@@ -15,6 +15,7 @@ img=img.resize((64,64))
 img=np.array(img)
 
 input_img=np.expand_dims(img,axis=0)
-result= np.argmax(model.predict(input_img),axis=1)
 
+#result= np.argmax(model.predict(input_img),axis=1)
+result = model.predict(input_img)
 print(result)
--- mainTrain.py
+++ mainTrain.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from keras.utils import normalize
 from keras.models import Sequential
 from keras.layers import Conv2D,MaxPooling2D,Activation,Dropout,Flatten,Dense
-from keras.utils import to_categorical
+#from keras.utils import to_categorical
 
 image_directories='datasets/'
 no_tumor_images=os.listdir(image_directories+'no/')
@@ -43,8 +43,8 @@ x_train, x_test,y_train,y_test=train_test_split(dataset,label,test_size=0.2,rand
 x_train=normalize(x_train,axis=1)
 x_test=normalize(x_test,axis=1)
 
-y_train=to_categorical(y_train,num_classes=2)
-y_test=to_categorical(y_test,num_classes=2)
+#y_train=to_categorical(y_train,num_classes=2)
+#y_test=to_categorical(y_test,num_classes=2)
 
 model=Sequential()
 model.add(Conv2D(32,(3,3), input_shape=(INPUT_SIZE,INPUT_SIZE,3)))
@@ -63,11 +63,11 @@ model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(2))
-model.add(Activation('softmax'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
 
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
 model.fit(x_train,y_train,batch_size=16,verbose=1,epochs=10,validation_data=(x_test,y_test),shuffle=False)
-model.save('BrainTumor10EpochsCategorical.h5')
+model.save('BrainTumor10Epochs.h5')
